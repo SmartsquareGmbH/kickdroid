48cbac9065a9f578af9eef624f05447d96036a08
Ruben Gees <rubengees7@googlemail.com>
2018-09-05 20:17:03 +0200

    Stop animation of loading indicator if invisible or gone

--- src/main/kotlin/de/smartsquare/kickdroid/view/LoadingIndicator.kt
+++ src/main/kotlin/de/smartsquare/kickdroid/view/LoadingIndicator.kt
@@ -3,6 +3,7 @@ package de.smartsquare.kickdroid.view
 import android.content.Context
 import android.content.res.Resources
 import android.util.AttributeSet
+import android.view.View
 import androidx.core.view.ViewCompat
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.iconics.IconicsDrawable
@@ -32,25 +33,35 @@ class LoadingIndicator @JvmOverloads constructor(
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
-        animateIndicator()
+        animateOrCancel()
     }
 
     override fun onDetachedFromWindow() {
-        ViewCompat.animate(this).cancel()
+        animateOrCancel()
 
         super.onDetachedFromWindow()
     }
 
-    private fun animateIndicator() {
-        ViewCompat.animate(this)
-            .x(Resources.getSystem().displayMetrics.widthPixels.toFloat())
-            .rotationBy(ROTATION)
-            .setDuration(ANIMATION_DURATION)
-            .withEndAction {
-                x = (-width).toFloat()
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
 
-                animateIndicator()
-            }
-            .start()
+        animateOrCancel()
+    }
+
+    private fun animateOrCancel() {
+        if (ViewCompat.isAttachedToWindow(this) && visibility == View.VISIBLE) {
+            ViewCompat.animate(this)
+                .x(Resources.getSystem().displayMetrics.widthPixels.toFloat())
+                .rotationBy(ROTATION)
+                .setDuration(ANIMATION_DURATION)
+                .withEndAction {
+                    x = (-width).toFloat()
+
+                    animateOrCancel()
+                }
+                .start()
+        } else {
+            ViewCompat.animate(this).cancel()
+        }
     }
 }
